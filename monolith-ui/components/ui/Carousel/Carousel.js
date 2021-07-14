51a93a2b2b36148c7d706d5bb8db559f8c9791e1
Daniel Chum <szewen.chum@gmail.com>
2021-07-14 17:54:42 +0800

    carousell without wrap around

--- monolith-ui/components/ui/Carousel/Carousel.js
+++ monolith-ui/components/ui/Carousel/Carousel.js
@@ -26,7 +26,7 @@ const Carousel = ({
   }
 
   return (
-    <div className='relative w-full h-60'>
+    <div className='relative w-full h-60 overflow-hidden'>
       <button
         className='z-50 absolute top-1/2 transform -translate-y-2/4 left-3  focus:outline-none text-white'
         onClick={() => plusNumber(-1)}
@@ -34,13 +34,18 @@ const Carousel = ({
         &#10094;
       </button>
 
-      <ol className='relative flex w-full h-full'>
-        { props.children.map((child, index) => {
+      <ol
+        className='flex h-full transition-all duration-500 ease-in-out'
+        style={{
+          width: `${props.children.length * 100}%`,
+          transform: `translateX(-${selectedIndex * 100 / props.children.length}%)`
 
-          let visibility = index == selectedIndex ? 'left-0' : index > selectedIndex ? "-right-full" : "-left-full"
+        }}
+      >
+        {props.children.map((child, index) => {
 
           return React.cloneElement(child, {
-            className: cn(`${visibility} absolute w-full h-full transition-all duration-500 ease-in-out`)
+            className: 'relative w-full h-full',
             })
           })
         }
